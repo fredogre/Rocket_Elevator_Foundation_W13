3828408b73b05f6cb9b8a14a2ea5af918a86375d
Guillaume Jacques <guillaumejacques69@msn.com>
2018-10-03 08:12:35 -0400

    CSS & JS integration

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,25 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+#Rails.application.config.assets.precompile += %w( header-1.css )
+#Rails.application.config.assets.precompile += %w( color_scheme/darkblue.css )
+#Rails.application.config.assets.precompile += %w( guillaume.css )
+#Rails.application.config.assets.precompile += %w( css/layout.css )
+#Rails.application.config.assets.precompile += %w( css/essentials.css )
+#Rails.application.config.assets.precompile += %w( guillaume.css )
+#Rails.application.config.assets.precompile += %w( script_guillaume.js )
+#Rails.application.config.assets.precompile += %w( https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js )
+#Rails.application.config.assets.precompile += %w( https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js )
+#Rails.application.config.assets.precompile += %w( https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js )
+#Rails.application.config.assets.precompile += %w( https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css )
+
+#Rails.application.config.assets.precompile += %w( plugins/slider.revolution/css/settings.css )
+#Rails.application.config.assets.precompile += %w( plugins/slider.revolution/css/extralayers.css )
+#Rails.application.config.assets.precompile += %w( plugins/bootstrap/css/bootstrap.min.css )
+
+
+
+
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
