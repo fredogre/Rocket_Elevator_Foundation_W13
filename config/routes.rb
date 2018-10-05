68de47eb9c7cbb031a34dd053ee6836b169af936
Guillaume Jacques <guillaumejacques69@msn.com>
2018-10-05 11:27:43 -0400

    merge master max branch

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'pages/indextemp'
   post '/new_quote' => 'quotes#new_quote'
   resource :quotes
+  resource :employees
   
   root 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
