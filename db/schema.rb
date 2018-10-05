50f7209b12dbf7fdf0003452a0a03137436e7ca1
Miraclemax3 <42751933+Miraclemax3@users.noreply.github.com>
2018-10-05 07:02:25 -0400

    table employees apparait pas

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_04_214402) do
-
- 
+ActiveRecord::Schema.define(version: 2018_10_04_215319) do
 
   create_table "employees", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "Last_Name"
