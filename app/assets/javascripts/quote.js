1119e4cc2591dbfc255b5860c2ca53efb91fc83c
Guillaume Jacques <guillaumejacques69@msn.com>
2018-10-05 17:36:52 -0400

    tempo

--- app/assets/javascripts/quote.js
+++ app/assets/javascripts/quote.js
@@ -265,28 +265,25 @@ function shaftcalculation(){
         quote_nbshafttotal.innerText = ''
     } else {
         quote_nbshafttotal.innerText = resultNbElevator;
-        $(temp_nbshafttotal).val(resultNbElevator)
-        //temp_nbshafttotal.innerText = resultNbElevator;
+        temp_nbshafttotal.value = resultNbElevator;
     };
     if (!unitcost){
         quote_result_unitcost.innerText = ''
     } else {
         quote_result_unitcost.innerText = parseInt(unitcost);
-        $(temp_unitcost).val(parseInt(unitcost))
-        //temp_unitcost.innerText = parseInt(unitcost);
+        temp_unitcost.value = parseInt(unitcost);
     };
     if (!installcost){
         quote_result_installcost.innerText = ''
     } else {
         quote_result_installcost.innerText = parseInt(installcost);
-        //temp_installcost.innerText = parseInt(installcost);
-        $(temp_installcost).val(parseInt(installcost))
+        temp_installcost.value = parseInt(installcost);
+
     };
     if (!totalcost){
         quote_result_totalcost.innerText = ''
     } else {
         quote_result_totalcost.innerText = parseInt(totalcost);
-        $(temp_totalcost).val(parseInt(totalcost))
-        //temp_totalcost.innerText = parseInt(totalcost);
+        temp_totalcost.value = parseInt(totalcost);
     };
 };
