88429e4b531d2dc9a66e8beb172403a58afb35d8
YOUR NAME <YOUR@EMAIL.com>
2018-10-23 16:41:42 -0400

    slack api

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -1,5 +1,11 @@
+require 'rest-client'
+require 'json'
+
 class Elevator < ApplicationRecord
   belongs_to :column, foreign_key: "column_id"
+
+  validate :status_validation
+  
 
   def model_enum
     ['Standard', 'Premium', 'Excelium']
@@ -12,5 +18,34 @@ class Elevator < ApplicationRecord
   def status_enum
     ['Intervention', 'Active', 'Inactive']
   end
+  
+    def send_to_slack(message)
+      RestClient.post(
+        'https://hooks.slack.com/services/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', 
+        {
+          payload: {
+            channel: "elevator_operations",
+            text: "#{message}",
+            username: "rocketelevatormanagement",
+            icon_emoji: ":heart:"
+          }.to_json
+        }
+      )
+      end
+
+      def status_validation
+        puts "new value #{self.status}"
+        new_status = self.status
+        old_status = Elevator.find(self.id).status
+        puts "old value #{old_status}"
+          if new_status != old_status 
+          send_to_slack ("The Elevator #{self.id} with serial number #{self.serial_number} changed status from #{old_status} to #{new_status} at #{self.updated_at}")
+          end 
+         
+        
+      end
+      
 
 end
+  
+
