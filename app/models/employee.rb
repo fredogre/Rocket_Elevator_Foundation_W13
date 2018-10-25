e292fc462cf2b4863efd0f21900d676ce7e9f78a
Patrick Thibault <tiboclan@gmail.com>
2018-10-25 14:44:51 -0400

    Watson operating

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,6 +1,7 @@
 class Employee < ApplicationRecord
-    belongs_to :user, required: false
+    belongs_to :user, optional: true
     has_many :batteries
+    validates_uniqueness_of :user_id
     
     def name
      "#{self.first_name} #{self.last_name}"
